428c149fbe6c169ac61cd72960e7529a73a72bd0
Wak Dyan <aditya.dyan@yahoo.com>
2019-08-14 09:11:59 +0700

    First commits

---
+++ lib/assets.dart
@@ -0,0 +1,13 @@
+var imgUrl = [
+  'https://i.pinimg.com/564x/80/a9/c0/80a9c005ba2f29555b1f3354b3c22b39.jpg',
+  'https://i.pinimg.com/564x/a7/52/ce/a752ce5fe7d96b95157642ea6eb55690.jpg',
+  'https://i.pinimg.com/564x/c9/5c/33/c95c33968e4eeac6587a8f423bf3bbb2.jpg',
+  'https://i.pinimg.com/564x/c4/57/bd/c457bd9496170bfa3845b7cee775df65.jpg'
+];
+
+var imgTitle = [
+'Smothered Baked Chicken Burritos',
+'White Dipping Sauce',
+'Tequila Kombucha Cocktail',
+'The Aficionado Cocktail'
+];
