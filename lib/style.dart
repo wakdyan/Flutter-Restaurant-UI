de3639ab01a6da8f0f9bf315a418fa71b725a1be
Wak Dyan <aditya.dyan@yahoo.com>
2019-08-19 09:09:55 +0700

    Add style

--- lib/style.dart
+++ lib/style.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 
-TextStyle title = (TextStyle(
+TextStyle mainTitleStyle = (TextStyle(
   fontSize: 28,
   fontWeight: FontWeight.w600,
   color: Colors.black,
   letterSpacing: 1,
+));
+
+TextStyle titleDetailStyle = (TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+  color: Colors.black,
+  letterSpacing: 1,
+));
+
+TextStyle postDetailStyle = (TextStyle(
+  // fontSize: 15,
+  color: Colors.black45,
+  // letterSpacing: 1,
+));
+
+TextStyle highlightTextHomeStyle = (TextStyle(
+  color: Colors.black45,
+  fontSize: 13,
+));
+
+TextStyle titleHomeStyle = (TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
 ));
